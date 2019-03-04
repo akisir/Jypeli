a3cdba00de773678849726c5d62821c88cc5c5d6
Aki Sirviö <akisir@gmail.com>
2019-03-04 14:36:12 +0200

    Add files via upload
    This program shows how windows forms can be used with Jypeli games.

---
+++ JypeliWinForm/JypeliWinForm/Form1.cs
@@ -0,0 +1,52 @@
+﻿//Form1.cs 4.3.2019
+//by Aki Sirviö
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JypeliWinForm
+{
+    public partial class Form1 : Form
+    {
+        JypeliWinForm peli; //game class
+
+        // constructor not in use
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // constructor takes game class and save it for later use.
+        public Form1(JypeliWinForm peliolio)
+        {
+            peli = peliolio;
+            InitializeComponent();
+        }
+
+        // button click event add balloons in game class method
+        private void button1_Click(object sender, EventArgs e)
+        {
+            peli.LisaaPallo();
+            textBox1.Text = "You got the balloon!";
+        }
+
+        // button click event destroy balloons in game class method
+        private void button2_Click(object sender, EventArgs e)
+        {
+            peli.TuhoaPallot();
+            textBox1.Text = "Try again.";
+        }
+
+        // form closing event close game in game class method
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            peli.Sulje();
+        }
+    }
+}
---
+++ JypeliWinForm/JypeliWinForm/JypeliWinForm.cs
@@ -0,0 +1,61 @@
+﻿//JypeliWinForm.cs 4.3.2019
+//by Aki Sirviö
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using Jypeli;
+using Jypeli.Assets;
+using Jypeli.Controls;
+using Jypeli.Widgets;
+
+namespace JypeliWinForm
+{
+    public class JypeliWinForm : PhysicsGame
+    {
+        private List<PhysicsObject> pallot = new List<PhysicsObject>();
+
+        // open form and sets initial settings
+        public override void Begin()
+        {
+            Form1 lomake = new Form1(this);
+            Jypeli.Timer.SingleShot(1, delegate { lomake.Show(); });
+            Gravity = new Vector(0, 500);
+            Level.CreateBorders();
+
+            PhoneBackButton.Listen(ConfirmExit, "Lopeta peli");
+            Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
+        }
+
+        // adds balloons
+        public void LisaaPallo()
+        {
+            PhysicsObject pallo = new PhysicsObject(185, 200);
+            int xCoord = RandomGen.NextInt((int)Screen.Left+110, (int)Screen.Right-110);
+            pallo.Image = LoadImage("balloon");
+            pallo.X = xCoord;
+            pallo.Y = Screen.Bottom;
+            pallo.CanRotate = false;
+            pallo.CollisionIgnoreGroup = 1;
+            Add(pallo);
+
+            pallot.Add(pallo);
+        }
+
+        // destroy balloons
+        public void TuhoaPallot()
+        {
+            SoundEffect poksahdus = LoadSoundEffect("Explosion147");
+            foreach (PhysicsObject x in pallot)
+            {
+                x.Destroy();
+                poksahdus.Play();
+            }
+        }
+
+        // close program
+        public void Sulje()
+        {
+            Exit();
+        }
+    }
+}
